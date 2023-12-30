2e8d7c99f98b64a503ec7aacfdc9f3177698d298
Ben96 <avazravshan0@gmail.com>
2023-12-30 16:58:53 +0900

    blog content page added

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,26 @@
+import BlogContent from "@components/BlogContent";
 import Hero from "@components/Hero";
+import { client } from "@sanity/lib/client";
+import { groq } from "next-sanity";
 
-export default function Home() {
+//================================================================================================================
+//Page revalidation time so that paage content would get updated every 30 seconds
+export const revalidate = 30;
+//================================================================================================================
+const query = groq`*[_type == "post"]{
+  ...,
+  author ->,
+    categories[] ->
+} | order(_createdAt asc)`;
+//================================================================================================================
+export default async function Home() {
+  const post = await client.fetch(query);
+  console.log(post);
   return (
     <main>
       <Hero />
+      <BlogContent posts={post} />
     </main>
   );
 }
+//================================================================================================================
