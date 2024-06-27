25c5be557ea2d78f485b464748933378b84dc805
Ben96 <avazravshan0@gmail.com>
2024-06-27 16:36:12 +0900

    object literal issue fix

--- sanity/schemas/blockContent.ts
+++ sanity/schemas/blockContent.ts
@@ -23,38 +23,40 @@ export default defineType({
       // set corresponds with HTML tags, but you can set any title or value
       // you want, and decide how you want to deal with it where you want to
       // use your content.
-      styles: [
-        { title: "Normal", value: "normal" },
-        { title: "H1", value: "h1" },
-        { title: "H2", value: "h2" },
-        { title: "H3", value: "h3" },
-        { title: "H4", value: "h4" },
-        { title: "Quote", value: "blockquote" },
-      ],
-      lists: [{ title: "Bullet", value: "bullet" }],
-      // Marks let you mark up inline text in the Portable Text Editor
-      marks: {
-        // Decorators usually describe a single property – e.g. a typographic
-        // preference or highlighting
-        decorators: [
-          { title: "Strong", value: "strong" },
-          { title: "Emphasis", value: "em" },
+      options: {
+        styles: [
+          { title: "Normal", value: "normal" },
+          { title: "H1", value: "h1" },
+          { title: "H2", value: "h2" },
+          { title: "H3", value: "h3" },
+          { title: "H4", value: "h4" },
+          { title: "Quote", value: "blockquote" },
         ],
-        // Annotations can be any object structure – e.g. a link or a footnote.
-        annotations: [
-          {
-            title: "URL",
-            name: "link",
-            type: "object",
-            fields: [
-              {
-                title: "URL",
-                name: "href",
-                type: "url",
-              },
-            ],
-          },
-        ],
+        lists: [{ title: "Bullet", value: "bullet" }],
+        // Marks let you mark up inline text in the Portable Text Editor
+        marks: {
+          // Decorators usually describe a single property – e.g. a typographic
+          // preference or highlighting
+          decorators: [
+            { title: "Strong", value: "strong" },
+            { title: "Emphasis", value: "em" },
+          ],
+          // Annotations can be any object structure – e.g. a link or a footnote.
+          annotations: [
+            {
+              title: "URL",
+              name: "link",
+              type: "object",
+              fields: [
+                {
+                  title: "URL",
+                  name: "href",
+                  type: "url",
+                },
+              ],
+            },
+          ],
+        },
       },
     }),
     // You can add additional types here. Note that you can't use
@@ -62,28 +64,21 @@ export default defineType({
     // as a block type.
     defineArrayMember({
       type: "image",
-      options: { hotspot: true },
-      fields: [
-        {
-          name: "alt",
-          type: "string",
-          title: "Alternative Text",
-        },
-      ],
+      options: {
+        hotspot: true,
+        fields: [
+          {
+            name: "alt",
+            type: "string",
+            title: "Alternative Text",
+          },
+        ],
+      },
     }),
     // Adding code block type
     defineArrayMember({
       type: "code",
       title: "Code Block",
-      options: {
-        language: "javascript",
-        languageAlternatives: [
-          { title: "JavaScript", value: "javascript" },
-          { title: "HTML", value: "html" },
-          { title: "CSS", value: "css" },
-          { title: "Python", value: "python" },
-        ],
-      },
     }),
   ],
 });
