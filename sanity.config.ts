2a1cc83b9243f69da485e59ae47f4606c78b039e
Ben96 <avazravshan0@gmail.com>
2024-06-27 16:21:46 +0900

    sanity code input added

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,7 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.tsx` route
- */
-
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import { codeInput } from "@sanity/code-input";
 import StudioNavbar from "@components/StudioNavbar";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
@@ -19,6 +16,7 @@ export default defineConfig({
   schema,
   plugins: [
     deskTool(),
+    codeInput(), // Add the codeInput plugin here
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
