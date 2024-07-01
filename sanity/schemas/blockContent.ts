c3a12647ad1d064b8717b679d4d3d2879ea6815b
Ben96 <avazravshan0@gmail.com>
2024-07-01 16:51:36 +0900

    verce object literal issue fix

--- sanity/schemas/blockContent.ts
+++ sanity/schemas/blockContent.ts
@@ -1,16 +1,5 @@
 import { defineType, defineArrayMember } from "sanity";
 
-/**
- * This is the schema type for block content used in the post document type
- * Importing this type into the studio configuration's `schema` property
- * lets you reuse it in other document types with:
- *  {
- *    name: 'someName',
- *    title: 'Some title',
- *    type: 'blockContent'
- *  }
- */
-
 export default defineType({
   title: "Block Content",
   name: "blockContent",
@@ -23,40 +12,38 @@ export default defineType({
       // set corresponds with HTML tags, but you can set any title or value
       // you want, and decide how you want to deal with it where you want to
       // use your content.
-      options: {
-        styles: [
-          { title: "Normal", value: "normal" },
-          { title: "H1", value: "h1" },
-          { title: "H2", value: "h2" },
-          { title: "H3", value: "h3" },
-          { title: "H4", value: "h4" },
-          { title: "Quote", value: "blockquote" },
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "H1", value: "h1" },
+        { title: "H2", value: "h2" },
+        { title: "H3", value: "h3" },
+        { title: "H4", value: "h4" },
+        { title: "Quote", value: "blockquote" },
+      ],
+      lists: [{ title: "Bullet", value: "bullet" }],
+      // Marks let you mark up inline text in the Portable Text Editor
+      marks: {
+        // Decorators usually describe a single property – e.g. a typographic
+        // preference or highlighting
+        decorators: [
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
         ],
-        lists: [{ title: "Bullet", value: "bullet" }],
-        // Marks let you mark up inline text in the Portable Text Editor
-        marks: {
-          // Decorators usually describe a single property – e.g. a typographic
-          // preference or highlighting
-          decorators: [
-            { title: "Strong", value: "strong" },
-            { title: "Emphasis", value: "em" },
-          ],
-          // Annotations can be any object structure – e.g. a link or a footnote.
-          annotations: [
-            {
-              title: "URL",
-              name: "link",
-              type: "object",
-              fields: [
-                {
-                  title: "URL",
-                  name: "href",
-                  type: "url",
-                },
-              ],
-            },
-          ],
-        },
+        // Annotations can be any object structure – e.g. a link or a footnote.
+        annotations: [
+          {
+            title: "URL",
+            name: "link",
+            type: "object",
+            fields: [
+              {
+                title: "URL",
+                name: "href",
+                type: "url",
+              },
+            ],
+          },
+        ],
       },
     }),
     // You can add additional types here. Note that you can't use
@@ -79,7 +66,6 @@ export default defineType({
     defineArrayMember({
       type: "code",
       title: "Code Block",
-      
       options: {
         language: "typescript",
         withFilename: true,
