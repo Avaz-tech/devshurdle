162c5a5b59823c3175990a4e15f613fbaaa17f5a
Ben96 <avazravshan0@gmail.com>
2024-07-12 15:02:10 +0900

    blog content: card ui modify

--- components/BlogContent.tsx
+++ components/BlogContent.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const BlogContent = ({ posts }: Props) => {
   return (
-    <Container className=" py-20 px-10 flex flex-col gap-10">
+    <Container className="py-20 mx-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-10">
       {posts.map((post) => (
         <Link
           key={post._id}
@@ -18,38 +18,38 @@ const BlogContent = ({ posts }: Props) => {
             query: { slug: post?.slug?.current },
           }}
         >
-          <div className="flex flex-col md:flex-row gap-10 rounded-md rounded-tl-md rounded-br-md shadow-md duration-200">
-            <div className="w-full md:w-3/5 group overflow-hidden rounded-tl-md rounded-bl-md relative">
+          <div className=" h-full border border-mainColor border-opacity-50 flex flex-col gap-[22px] justify-between rounded-md shadow-custom-card duration-200">
+            <div className="w-full h-[225px] md:w-full group overflow-hidden rounded-md relative">
               <Image
                 src={urlForImage(post.mainImage).url()}
-                width={500}
-                height={500}
+                width={200}
+                height={200}
                 alt="blog post image"
                 className="w-full h-full object-cover group-hover:scale-105 duration-500 rounded-tl-md rounded-bl-md"
                 priority={true}
               />
               <div className="absolute top-0 left-0 bg-black/20 w-full h-full group-hover:hidden duration-200" />
-              <div className="absolute hidden group-hover:inline-flex bottom-0 left-0 w-full bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-white p-5 justify-center duration-200">
+              <div className="absolute hidden group-hover:inline-flex bottom-0 left-0 w-full bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-mainColor p-5 justify-center duration-200">
                 <p className="text-lg font-semibold">Click to Read</p>
               </div>
             </div>
-            <div className="w-full md:w-2/5 flex flex-col justify-between py-10 px-4">
+            <div className="w-full grow flex flex-col gap-3 justify-between p-4">
               <div className="flex flex-col gap-5">
                 {/* ------------------------------------ */}
                 <div className="flex items-center gap-2 ">
                   {post?.categories?.map((item) => (
-                    <p key={item._id} className="text-xs uppercase text-green-600 font-semibold">
+                    <p key={item._id} className="text-xs uppercase text-green-600 border rounded-full py-2 px-3 font-semibold">
                       {item?.title}
                     </p>
                   ))}
                 </div>
                 {/* ------------------------------------ */}
-                <h2 className="text-2xl font-semibold hover:text-orange-600 duration-200 cursor-pointer">{post?.title}</h2>
+                <h2 className="text-xl font-semibold hover:text-orange-600 duration-200 cursor-pointer">{post?.title}</h2>
                 {/* ------------------------------------ */}
-                <p className="text-gray-500">{post?.description}</p>
+                {/* <p className="text-gray-500">{post?.description}</p> */}
                 {/* ------------------------------------ */}
               </div>
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between mt-3">
                 <p className="text-sm font-semibold text-gray-500">
                   {new Date(post?._createdAt).toLocaleDateString("en-US", {
                     day: "numeric",
--- components/Container.tsx
+++ components/Container.tsx
@@ -5,7 +5,7 @@ interface Props{
 
 const Container = ({children, className}: Props) => {
   return (
-    <div className={`max-w-screen-xl mx-auto  ${className}`}>{children}</div>
+    <div className={`max-w-screen-xl ${className}`}>{children}</div>
   )
 }
 
