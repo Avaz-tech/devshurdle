ebb63b4d7392d1d69fe8b2e486a2a27a067d0323
Ben96 <avazravshan0@gmail.com>
2024-10-25 00:08:53 +0900

    useCdn based on environment

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from "next-sanity";
-
 import { apiVersion, dataset, projectId, useCdn } from "../env";
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn: false,
+  useCdn: useCdn, // Use CDN based on your environment
+  token: process.env.SANITY_TOKEN, // Optional if dataset is public
 });
