17d1eff32cfcb57503173d186159f894606c3413
Ben96 <avazravshan0@gmail.com>
2024-11-28 16:47:12 +0900

    fix toggle filter -> destructure { categories: title } for exact match

--- components/SearchPanel.tsx
+++ components/SearchPanel.tsx
@@ -4,17 +4,28 @@ import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { useSearchContext } from "@app/Context/SearchContext";
 import { useState, useEffect } from "react";
 
+interface Category {
+  title: string;
+  description: string;
+}
+
+interface Post {
+  categories: Category[];
+}
 const filterOptions = [
-  { label: "Latest", value: "latest" },
   { label: "Browser", value: "browser" },
   { label: "Style", value: "style" },
   { label: "Config", value: "config" },
-  { label: "Common", value: "common" },
+  { label: "Type", value: "type" },
+  { label: "Performance", value: "performance" },
+  { label: "API", value: "api" },
 ];
 
 const SearchPanel: React.FC = () => {
-  const { setFilteredItems, filteredItems, originalItems } = useSearchContext();
+  const { setFilteredItems, originalItems } = useSearchContext();
   const [activeFilters, setActiveFilters] = useState<string[]>([]); // Track active filters
+
+  console.log("Oringinal Items:", originalItems);
 
   useEffect(() => {
     if (activeFilters.length === 0) {
@@ -22,10 +33,9 @@ const SearchPanel: React.FC = () => {
       setFilteredItems(originalItems);
     } else {
       // If filters are active, filter posts based on selected filters
-      const filteredP = originalItems.filter((_post: { title: string; description: string }) => {
-        return activeFilters.some((filter) =>
-          _post.title.toLowerCase().includes(filter.toLowerCase()) ||
-          _post.description.toLowerCase().includes(filter.toLowerCase())
+      const filteredP = originalItems.filter((_post: Post) => {
+        return _post.categories?.some((category) =>
+          activeFilters.some((filter) => category.title?.toLowerCase().includes(filter.toLowerCase()))
         );
       });
       setFilteredItems(filteredP);
@@ -34,10 +44,11 @@ const SearchPanel: React.FC = () => {
 
   const handleToggle = (value: string) => {
     // Toggle filter on/off
-    setActiveFilters((prevFilters) =>
-      prevFilters.includes(value)
-        ? prevFilters.filter((filter) => filter !== value) // Remove filter
-        : [...prevFilters, value] // Add filter
+    setActiveFilters(
+      (prevFilters) =>
+        prevFilters.includes(value)
+          ? prevFilters.filter((filter) => filter !== value) // Remove filter
+          : [...prevFilters, value] // Add filter
     );
   };
 
